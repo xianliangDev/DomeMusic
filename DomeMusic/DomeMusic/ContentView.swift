4c1a7e1d37f89a43061399133c0ba3ba91bbd14f
xianliangDev <xianliangDev@163.com>
2021-10-31 17:58:24 +0800

    init dome music pro

---
+++ DomeMusic/DomeMusic/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DomeMusic
+//
+//  Created by xianliangDev on 2021/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @available(iOS 13.0.0, *)
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    @available(iOS 13.0.0, *)
+    static var previews: some View {
+        ContentView()
+    }
+}
